a84ce55e03872711b91ae2ad5a74ea2848121a4a
chiache <chiache7410@gmail.com>
2016-11-24 16:34:50 +0800

    2016-11-24_app >> html

--- app/src/main/java/tw/org/iii/lab16/MainActivity.java
+++ app/src/main/java/tw/org/iii/lab16/MainActivity.java
@@ -2,7 +2,10 @@ package tw.org.iii.lab16;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -14,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
         initWebView();
     }
     private void initWebView(){
+        MyWebViewClient client = new MyWebViewClient();
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(client);
         // 1.
-        webView.loadUrl("http://www.iii.org.tw");
+        //webView.loadUrl("http://www.iii.org.tw");
         // 2.
-
+        webView.loadUrl("file:///android_asset/page.html");
+        // 3.
+        //webView.loadData("<h1>Brad Company</h1>","text/html;charset=utf-8",null);
+    }
+    private class MyWebViewClient extends WebViewClient {
+    }
+    public void test1(View v){
+        String name = "Eric";
+        webView.loadUrl("javascript:test1('" + name + "')");
     }
 }
